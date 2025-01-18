b1d0ddb957ec9d9589989b3ce84de967a4f17c35
Farbod909 <rafezyfarbod@gmail.com>
2025-01-18 15:44:20 -0800

    Fix name of test case

--- authn/tests.py
+++ authn/tests.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 
-class UsersManagersTests(TestCase):
+class UserManagerTests(TestCase):
 
     def test_create_user(self):
         User = get_user_model()
